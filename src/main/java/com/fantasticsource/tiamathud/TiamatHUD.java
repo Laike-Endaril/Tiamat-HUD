86e3fd65ef77c96bbdd51a13d092fd852ae7f04a
Kyle Koder <silvertallon@gmail.com>
2020-07-19 12:11:16 -0700

    Initial commit

---
+++ src/main/java/com/fantasticsource/tiamathud/TiamatHUD.java
@@ -0,0 +1,29 @@
+package com.fantasticsource.tiamathud;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = TiamatHUD.MODID, name = TiamatHUD.NAME, version = TiamatHUD.VERSION)
+public class TiamatHUD
+{
+    public static final String MODID = "tiamathud";
+    public static final String NAME = "Tiamat HUD";
+    public static final String VERSION = "1.12.2.000";
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+        MinecraftForge.EVENT_BUS.register(TiamatHUD.class);
+    }
+
+    @SubscribeEvent
+    public static void saveConfig(ConfigChangedEvent.OnConfigChangedEvent event)
+    {
+        if (event.getModID().equals(MODID)) ConfigManager.sync(MODID, Config.Type.INSTANCE);
+    }
+}
