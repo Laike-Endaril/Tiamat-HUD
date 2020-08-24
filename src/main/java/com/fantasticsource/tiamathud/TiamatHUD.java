980488963f24cd7af3aeb0753bab91a29341a973
Kyle Koder <silvertallon@gmail.com>
2020-08-24 12:00:10 -0700

    1.12.2.000i

--- src/main/java/com/fantasticsource/tiamathud/TiamatHUD.java
+++ src/main/java/com/fantasticsource/tiamathud/TiamatHUD.java
@@ -12,7 +12,7 @@ public class TiamatHUD
 {
     public static final String MODID = "tiamathud";
     public static final String NAME = "Tiamat HUD";
-    public static final String VERSION = "1.12.2.000h";
+    public static final String VERSION = "1.12.2.000i";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
